694c20e582ff202833b86622205c70ba1a40ddc6
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-11-19 20:06:40 +0300

    create book repo imp

--- lib/features/home/data/repository/book_repo.dart
+++ lib/features/home/data/repository/book_repo.dart
@@ -3,7 +3,7 @@ import 'package:bookly_app/features/home/data/model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class BookRepo {
-  Future<Either<Failure, BookModel>> getAllBooks();
+  Future<Either<Failure, List<BookModel>>> getAllBooks();
 
-  Future<Either<Failure, BookModel>> getBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> getBestSellerBooks();
 }
---
+++ lib/features/home/data/repository/book_repo_imp.dart
@@ -0,0 +1,39 @@
+import 'package:bookly_app/core/error/exceptions.dart';
+import 'package:bookly_app/core/error/failures.dart';
+import 'package:bookly_app/core/services/api_service.dart';
+import 'package:bookly_app/features/home/data/model/book_model.dart';
+import 'package:bookly_app/features/home/data/repository/book_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class BookRepoImp implements BookRepo {
+  final ApiService data;
+
+  BookRepoImp(this.data);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> getAllBooks() async {
+    try {
+      final result = await data.get(
+        endPoints: '/volumes?Filtering=free-ebooks&Sorting=newest',
+      );
+      final List<BookModel> book = [];
+      for (var e in result['items']) {
+        book.add(BookModel.fromJson(e));
+      }
+      return right(book);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      } else {
+        return Left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> getBestSellerBooks() {
+    // TODO: implement getBestSellerBooks
+    throw UnimplementedError();
+  }
+}
