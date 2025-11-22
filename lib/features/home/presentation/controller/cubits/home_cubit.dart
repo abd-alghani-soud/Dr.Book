96705006226d849d23942ef6751e182398e474f8
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-11-22 17:41:37 +0300

    create home cubit 2

--- lib/features/home/presentation/controller/cubits/home_cubit.dart
+++ lib/features/home/presentation/controller/cubits/home_cubit.dart
@@ -3,19 +3,23 @@ import 'package:bookly_app/core/utils/request_status.dart';
 import 'package:bookly_app/features/home/data/model/book_model.dart';
 import 'package:bookly_app/features/home/data/repository/book_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
 
 part 'home_state.dart';
 
+@lazySingleton
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit(this.repo) : super(HomeState());
 
   final BookRepo repo;
 
   Future<void> getAllBooks() async {
+    print('load');
     emit(state.copyWith(getAllBooksState: RequestStatus.loading));
     final result = await repo.getAllBooks();
     result.fold(
       (failure) {
+        print('faileddddddd$failure');
         emit(
           state.copyWith(
             getAllBooksState: RequestStatus.failed,
@@ -24,6 +28,7 @@ class HomeCubit extends Cubit<HomeState> {
         );
       },
       (books) {
+        print('succcccccccccccccc$books');
         emit(
           state.copyWith(
             getAllBooksState: RequestStatus.success,
